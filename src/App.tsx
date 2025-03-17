e4d9f4faa28cc7bb963f0cd4acf768c900740607
Oleh Mysko <olegmysko1996@gmail.com>
2025-03-17 19:53:39 +0200

    winr

--- src/App.tsx
+++ src/App.tsx
@@ -25,10 +25,12 @@ export class App extends React.Component<{}, State> {
     return (
       <div className="App">
         <p className="App__message">
+
           {this.state.pressedKey
             ? `The last pressed key is [${this.state.pressedKey}]`
             : 'Nothing was pressed yet'
           }
+          
         </p>
       </div>
     );
