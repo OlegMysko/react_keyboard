390d26fe97681318b858b6a625cc87220728d8e7
Oleh Mysko <olegmysko1996@gmail.com>
2025-03-17 19:52:07 +0200

    winr

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
 import React from 'react';
 
-
 type State = {
   pressedKey: string | null;
 };
 
-
-export class App extends React.Component<{},State>{
+export class App extends React.Component<{}, State> {
   state: Readonly<State> = {
-pressedKey:null
+    pressedKey: null,
   };
+
   handleKeyUp = (event: KeyboardEvent) => {
     this.setState({ pressedKey: event.key });
   };
 
   componentDidMount() {
-    document.addEventListener('keyup', this.handleKeyUp)
+    document.addEventListener('keyup', this.handleKeyUp);
   }
+
   componentWillUnmount() {
-    document.removeEventListener('keyup', this.handleKeyUp)
+    document.removeEventListener('keyup', this.handleKeyUp);
   }
+
   render(): React.ReactNode {
     return (
       <div className="App">
         <p className="App__message">
           {this.state.pressedKey
             ? `The last pressed key is [${this.state.pressedKey}]`
-            : "Nothing was pressed yet"}
+            : 'Nothing was pressed yet'
+          }
         </p>
       </div>
     );
   }
-
 }
